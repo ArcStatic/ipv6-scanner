1952b987ba8fa86beaa3c6cccbef8b7b863da288
Vivian Band <vivianband0@gmail.com>
2021-12-08 10:55:07 +0000

    detect change in prefixes

--- libpcap_responses.c
+++ libpcap_responses.c
@@ -1,12 +1,3 @@
-/* ldev.c
- *    Martin Casado
- *       
- *          To compile:
- *             >gcc ldev.c -lpcap
- *
- *                Looks for an interface, and lists the network ip
- *                   and mask associated with that interface.
- *                   */
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
@@ -20,8 +11,10 @@
 #include <linux/if_ether.h>
 #include <dirent.h>
 
+#define PFX_STR_SIZE 15
+
 struct capture_info {
-    int total_pkt_count;
+    int total_resp_count;
     int echo_req_count;
     int echo_reply_count;
     int time_exceeded_count;
@@ -31,6 +24,15 @@ struct capture_info {
     int port_unreachable_count;
     int reject_route_count;
     int failed_policy_count;
+    struct pfx* current_pfx;
+};
+
+struct pfx {
+    char* pfx_addr;
+    int mask_len;
+    struct capture_info* info;
+    struct pfx* next;
+    struct pfx* prev;
 };
 
 
@@ -44,7 +46,10 @@ void print_packet_info(u_char *info, const u_char *packet, struct pcap_pkthdr pa
   icmpv6_header = packet + sizeof(struct ether_header) + sizeof(struct ip6_hdr);
 
   struct capture_info *i;
-  i = (struct capture_info *) info;     
+  i = (struct capture_info *) info;
+
+  char ipv6_addr_str[100];
+  char ipv6_str_slice[15];
 
   if (ntohs(eth_header->ether_type) == ETHERTYPE_IP) {
     printf("IP\n");
@@ -55,23 +60,42 @@ void print_packet_info(u_char *info, const u_char *packet, struct pcap_pkthdr pa
   } else if (ntohs(eth_header->ether_type) == ETHERTYPE_IPV6) {
       //ICMPv6 payload
       if (ipv6_header->ip6_nxt == 58) {
-	//printf("ICMPv6\n");
+        inet_ntop(AF_INET6, &ipv6_header->ip6_src, ipv6_addr_str, 100);
+        //inet_ntop(AF_INET6, &ipv6_header->ip6_src, ipv6_str_slice, 100);
+        printf("Addr: %s\n", ipv6_addr_str);
+	memcpy(ipv6_str_slice, ipv6_addr_str, 14);
+	ipv6_str_slice[15] = '\0';
+	//why is this call blank after memcpy?
+        //printf("Addr: %s\n", ipv6_addr_str);
+        if (i->current_pfx->pfx_addr == NULL){
+	  //
+	  i->current_pfx->pfx_addr = (char*) malloc(PFX_STR_SIZE);
+	  strcpy(i->current_pfx->pfx_addr, ipv6_str_slice);
+	} else if (strcmp(i->current_pfx->pfx_addr, ipv6_str_slice) != 0){
+	//} else if (i->current_pfx->pfx_addr != *(ipv6_str_slice)){
+	  strcpy(i->current_pfx->pfx_addr, ipv6_str_slice);
+	}	
+        printf("Slice: %s\n", i->current_pfx->pfx_addr);
+        printf("Str slice: %s\n", ipv6_str_slice);
+
+//printf("ICMPv6\n");
 	//printf("type: %d, ", icmpv6_header->icmp6_type);
 	//Check for Echo Request
 	if (icmpv6_header->icmp6_type == 128){
 	  //printf("Echo Req\n");
 	  i->echo_req_count++;
-          i->total_pkt_count++;
+	  //not a response, this is an outgoing probe
+          //i->total_resp_count++;
 	//Check for Echo Reply
 	} else if (icmpv6_header->icmp6_type == 129){
 	  //printf("Echo Reply\n");
 	  i->echo_reply_count++; 
-          i->total_pkt_count++;
+          i->total_resp_count++;
 	//Check for Time Exceeded
 	} else if (icmpv6_header->icmp6_type == 3){
 	  //printf("Time Exceeded\n");
 	  i->time_exceeded_count++;
-	  i->total_pkt_count++;
+	  i->total_resp_count++;
 	//Check for Destination Unreachable
 	} else if (icmpv6_header->icmp6_type == 1){
 	    //No Route
@@ -95,7 +119,7 @@ void print_packet_info(u_char *info, const u_char *packet, struct pcap_pkthdr pa
             } else {
 	       printf("Unhandled ICMPv6 unreachable code: %d\n", icmpv6_header->icmp6_code);
 	    }
-	    i->total_pkt_count++;
+	    i->total_resp_count++;
 	//Neighbour Solicitation - ignore
 	} else if (icmpv6_header->icmp6_type == 135){
 		;
@@ -124,6 +148,7 @@ int dir_loop(char *dirname, struct capture_info *info){
   struct pcap_pkthdr packet_header;
   pcap_t *handle = NULL;
   char abs_path[256];
+  struct pfx *current_pfx;
 
   folder = opendir(dirname);
   if (folder == NULL){
@@ -146,10 +171,10 @@ int dir_loop(char *dirname, struct capture_info *info){
 	//printf("Loop done.\n");
 	/*
 	printf(
-	  "Ongoing count:\nEcho Req: %d\nEcho Reply: %d\nTime Exceeded: %d\nNo Route: %d\nAddress Unreachable: %d\nAdmin Prohibited: %d\nPort Unreachable: %d\nReject Route: %d\nSrc Addr Failed Ingress/Egress Policy: %d\nTotal: %d\n\n", 
+	  "Ongoing count:\nEcho Req: %d\nEcho Reply: %d\nTime Exceeded: %d\nNo Route: %d\nAddress Unreachable: %d\nAdmin Prohibited: %d\nPort Unreachable: %d\nReject Route: %d\nSrc Addr Failed Ingress/Egress Policy: %d\nTotal responses: %d\n\n", 
 	  info->echo_req_count, info->echo_reply_count, info->time_exceeded_count, info->no_route_count, 
 	  info->address_unreachable_count, info->admin_prohibited_count, info->port_unreachable_count, 
-	  info->reject_route_count, info->failed_policy_count, info->total_pkt_count);
+	  info->reject_route_count, info->failed_policy_count, info->total_resp_count);
 	*/
         	
       } else {
@@ -168,7 +193,7 @@ int dir_loop(char *dirname, struct capture_info *info){
 int main(int argc, char **argv)
 { 
   struct capture_info info;
-  info.total_pkt_count = 0;
+  info.total_resp_count = 0;
   info.echo_req_count = 0;
   info.echo_reply_count = 0;
   info.time_exceeded_count = 0;
@@ -179,7 +204,37 @@ int main(int argc, char **argv)
   info.reject_route_count = 0;
   info.failed_policy_count = 0;
  
-  dir_loop(argv[1], &info);
+  struct pfx* current_pfx;
+
+  current_pfx = (struct pfx*)malloc(sizeof(struct pfx));
+  current_pfx->pfx_addr = NULL;
+  current_pfx->mask_len = 0;
+  current_pfx->prev = NULL;
+  current_pfx->next = NULL;
+  current_pfx->info = (struct capture_info*)malloc(sizeof(struct capture_info));
+
+  info.current_pfx = current_pfx;
+  
+  //populate info struct with data from both send and receive captures:
+  for (int i = 1; i <= argc - 1; i++){
+    if (strstr(argv[i], "send") != NULL){
+	//printf("Send dir: %s\n", argv[i]);
+        dir_loop(argv[i], &info);
+    } else if (strstr(argv[i], "recv") != NULL){
+	//printf("Recv dir: %s\n", argv[i]);
+	dir_loop(argv[i], &info);
+    }
+  }
+  
+  
+  
+  printf(
+	  "Ongoing count:\nEcho Req: %d\nEcho Reply: %d\nTime Exceeded: %d\nNo Route: %d\nAddress Unreachable: %d\nAdmin Prohibited: %d\nPort Unreachable: %d\nReject Route: %d\nSrc Addr Failed Ingress/Egress Policy: %d\nTotal responses received: %d\n\n", 
+	  info.echo_req_count, info.echo_reply_count, info.time_exceeded_count, info.no_route_count, 
+	  info.address_unreachable_count, info.admin_prohibited_count, info.port_unreachable_count, 
+	  info.reject_route_count, info.failed_policy_count, info.total_resp_count
+  );
+
 
   return 0;
 }
